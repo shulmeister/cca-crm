aa8e82558b7d5ad2a337f52741d28bd7096facd7
shulmeister <jason@coloradocareassist.com>
2025-11-14 14:00:06 -0700

    Update branding to Colorado CareAssist

--- src/components/atomic-crm/root/defaultConfiguration.ts
+++ src/components/atomic-crm/root/defaultConfiguration.ts
@@ -1,9 +1,9 @@
 import { Mars, NonBinary, Venus } from "lucide-react";
 
-export const defaultDarkModeLogo = "./logos/logo_atomic_crm_dark.svg";
-export const defaultLightModeLogo = "./logos/logo_atomic_crm_light.svg";
+export const defaultDarkModeLogo = "./logos/logo_colorado_careassist_dark.svg";
+export const defaultLightModeLogo = "./logos/logo_colorado_careassist_light.svg";
 
-export const defaultTitle = "Atomic CRM";
+export const defaultTitle = "Colorado CareAssist CRM";
 
 export const defaultCompanySectors = [
   "Communication Services",
